bd323773608eef8ad6522ae7c8135793fe3caf04
zypharionstudios <zypharionstudios@gmail.com>
2026-09-13 16:17:49 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+group = "de.echosmp"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    // Paper API 1.21+ – wird vom Server bereitgestellt, daher compileOnly
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
